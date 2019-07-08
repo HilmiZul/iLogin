3018de3da159add320d604ab2ad82fdf653bfa91
hilmizul <netspytux@gmail.com>
2019-07-08 14:11:33 +0700

    ...

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var pinField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnLogin(_ sender: UIButton) {
+        
+        let email : String = emailField.text!
+        let pin : String = pinField.text!
+        
+        if email.isEmpty || pin.isEmpty {
+            let alert = UIAlertController(title: "Error", message: "Email/PIN Harus Diisi!", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            return;
+        }
+        
+        if (email == "halo@zul.id") && (pin == "123") {
+            let alert = UIAlertController(title: "Sukses", message: "Login Berhasil!", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+            present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Gagal", message: "Email/PIN salah", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
 }
 
